89bfe309704d2aebcbad41bc093828e5a2c2dc30
dezsi.lajos_bankitat <dezsi.lajos@bankitatabanya.hu>
2025-10-13 11:34:06 +0200

    direction, item, scene

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,6 @@
 public class Game {
     public static void main(String[] args) {
 
-        // git remote add origin https://github.com/lovedepment/worldoflali2.git
+
     }
 }
---
+++ src/Scene.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Scene {
+    private String description;
+    private ArrayList<Item> items;
+    private HashMap<Direction,Scene> exits;
+
+    public Scene(String description) {
+        this.description = description;
+        this.items = new ArrayList<>();
+        this.exits = new HashMap<>();
+    }
+
+    public void setExit(Direction direction, Scene neighbor) {
+        //itt addunk hozzá kijáratot a helyszínhez, irány és szomszéd helyszín formájában
+        // A HashMap put() metódusa az hasonló az arrayList add() metódusához, új elemet addunk hozzá
+        // csak ez key (dirtection) és value(szomszéd) párokat vár mindig. A Direction egx ENUM
+        exits.put(direction, neighbor);
+    }
+    public void addItem (Item item) {
+        items.add(item);
+    }
+    //az adott irányban milyen szomszédos helyszín van
+    public Scene getExit(Direction direction) {
+        return exits.get(direction);
+    }
+
+}
