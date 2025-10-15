7811694924cff6e24a758efa649e6714a4508d23
dezsi.lajos_bankitat <dezsi.lajos@bankitatabanya.hu>
2025-10-15 13:07:55 +0200

    play and display methods, game loop with scanner

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Game {
+
     public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+    }
 
+    private void play() {
+        Scanner scanner = new Scanner(System.in);
+        display("Üdv a várkalandban");
 
+        //ez maga játék ciklus
+        while(true){
+            String input = scanner.nextLine().toLowerCase(Locale.ROOT).trim();
+            String command = input;
+
+            switch (command) {
+
+                case "kilep":
+                    display("Köszi a játékot!");
+                    scanner.close();
+                    return;
+            }
+
+        }
+    }
+
+    private void display(String message) {
+        System.out.println(message);
     }
 }
